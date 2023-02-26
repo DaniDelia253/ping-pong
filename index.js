167ccb89aa7c3fdcf94bd18600d8e37808a831fb
Kirsten Davis <kirstendavis@Kirstens-MacBook-Air.local>
2023-02-26 11:28:00 -0600

    react to cat

--- index.js
+++ index.js
@@ -13,17 +13,29 @@ const client = new Client({
 		GatewayIntentBits.MessageContent,
 		//with these three, I can gain access to text in messages
 		//if i wanted info about reactions, I would have to specify that here!!
-		//!if something isn't working, most likely the right info is not included here!
+		//if something isn't working, most likely the right info is not included here!
 	],
 });
 
 //to make sure we know when the bot is ready
 client.on("ready", () => {
-	console.log("The bot is ready :)");
+	console.log("The bot is ready :)))))");
 });
 
 //so basically, look at every new message.....
-client.on("messageCreate", async (message) => {});
+client.on("messageCreate", async (message) => {
+	//if the message is from dani and has the words "has reached level"
+	if (
+		//todo change this to arcane when ready for NVO 
+		message.content.includes("cat")
+	){
+		//then i want it to react with a random emoji from a list 
+		const list = ["1078506917812506624", "1079448316321992715", "1079448313385996338", "1079448310982651975", "1079448308604485743", "1078508965098422417"]
+		const number = Math.floor(Math.random()*6)
+		message.react(list[number])
+	}
+});
 
 // log in and auhtenticate
 client.login(process.env.TOKEN);
+//"1078506917812506624" "1079448316321992715" "1079448313385996338" "1079448310982651975" "1079448308604485743"
