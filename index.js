ccd78a8f54025f5a21f95d0c2efa9ccb447df564
DaniDela253 <danidavis321@gmail.com>
2023-03-11 09:28:55 -0600

    Co-authored-by: stenny <stennysaurus@users.noreply.github.com>

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ import fetch from "node-fetch";
 //todo example orange
 //!    example red
 //?    example blue
-//*    example green 
+//*    example green
 //~~   example strikethough
 
-//look at the docs for ALLLL issues!!
+//* All of our important stuff:
+const GuildIDs = {
+	DaniDeliaServer: "1078490032324759642",
+	Server: "1048059530711404615",
+	NVO: "938105437180551168",
+};
+
+const ChannelTags = {
+	NVOrules: "<#938916275437064213>",
+	NVOroles: "<#942151699718602773>",
+	NVOanythingGoes: "<#938105437180551172>",
+};
+
+//automatically set to stenny.... changed when someone joins:
+let newlyJoinedMemberId = "876850436953481277";
 
 //client obj represents the entire bot:
 const client = new Client({
@@ -18,7 +32,8 @@ const client = new Client({
 		GatewayIntentBits.Guilds,
 		GatewayIntentBits.GuildMessages,
 		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMessageReactions
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMembers,
 		//with these three, I can gain access to text in messages
 		//if i wanted info about reactions, I would have to specify that here!!
 		//if something isn't working, most likely the right info is not included here!
@@ -32,9 +47,9 @@ client.on("ready", () => {
 
 //so basically, look at every new message.....
 client.on("messageCreate", async (message) => {
-	//if the message is from dani, stenny or Arcane and has the words "has reached level"
+	//if the message is from dani, stenny or Arcane and has the words "has reached level", then react with an emoji from the list
 	if (
-		//todo add/replace emojies, and change the random number limit accordingly. 
+		//todo add/replace emojies, and change the random number limit accordingly.
 		message.content.includes("cat") ||
 		((message.author.username === "Arcane" ||
 			message.author.username === "DaniDelia" ||
@@ -63,13 +78,14 @@ client.on("messageCreate", async (message) => {
 		message.channel.send("youre welcome!");
 	}
 	//todo (fix: format and make it apply to stenny too), goodnight message
-	if (message.author.username === "DaniDelia" &&
-		message.content.includes("good night ping pong")) {
+	if (
+		message.author.username === "DaniDelia" &&
+		message.content.includes("good night ping pong")
+	) {
 		//i want him to say goodnight back, specifying the person
-			message.channel.send("good night, dani!");
+		message.channel.send("good night, dani!");
 	}
 
-	
 	//and if it contains a number followed by a space and teh word fact....
 	if (
 		/[0-9]+\ (fact)/.test(message.content) &&
@@ -101,7 +117,7 @@ client.on("messageCreate", async (message) => {
 		message.author.username !== "ping-pong" &&
 		message.content !== "thank you ping pong" &&
 		//todo message.content !== "show me a cat"   (   THERES A BETTER WAY APPARENTLY! :)   )
-(!message.content.includes("show me a cat"))
+		!message.content.includes("show me a cat")
 	) {
 		//go through every single letter of the message..... and....
 		let response = "";
@@ -130,31 +146,73 @@ client.on("messageCreate", async (message) => {
 		//send the response
 		message.channel.send(response);
 	}
-})
-client.on("messageReactionAdd",async (reaction) =>{
-		// if (reaction === )
-		//this is for Server
-		if (reaction._emoji.id === "1079573401888358510"){
-			// message.react("1079573401888358510")
-			const channel = await client.channels.fetch(reaction.message.channelId)
-			channel.messages.fetch(reaction.message.id).then(msg=>{
-				msg.react("1079573401888358510")
-			})
-		}
-	})
-client.on("messageReactionAdd",async (reaction) =>{
-		// if (reaction === )
-		//this is for NVO
-		if (reaction._emoji.id === "1079573678515298344"){
-			// message.react("1079573401888358510")
-			const channel = await client.channels.fetch(reaction.message.channelId)
-			channel.messages.fetch(reaction.message.id).then(msg=>{
-				msg.react("1079573678515298344")
-			})
-		}
-})
+
+	// const embedMessage = {
+	// 	type: "rich",
+	// 	title: `✿﹒welcome  ﹒✿`,
+	// 	description: `Welcome <@${newlyJoinedMemberId}> to NVO,  it's lovely to have you here!\n‎\n﹒verify our ${ChannelTags.NVOrules} & grab some ${ChannelTags.NVOroles}\n\n﹒then come hang out with us in ${ChannelTags.NVOanythingGoes}!\n‎`,
+	// 	color: 0xedb2aa,
+	// 	image: {
+	// 		url: `https://i.imgur.com/7m2LO4M.png`,
+	// 		height: 0,
+	// 		width: 0,
+	// 	},
+	// };
+	// if (message.content.includes("test the welcome message embed")) {
+	// 	message.channel.send({ embeds: [embedMessage] });
+	// }
+});
+
+//welcome messagre for when new members join in:
+client.on("guildMemberAdd", async (member) => {
+	//when a new member joins happens, get their id and establish all variables we need:
+	newlyJoinedMemberId = member.user.id;
+	//create the embed to send:
+	const embedMessage = {
+		type: "rich",
+		title: `✿﹒welcome  ﹒✿`,
+		description: `Welcome <@${newlyJoinedMemberId}> to NVO,  it's lovely to have you here!\n‎\n﹒verify our ${ChannelTags.NVOrules} & grab some ${ChannelTags.NVOroles}\n\n﹒then come hang out with us in ${ChannelTags.NVOanythingGoes}!\n‎`,
+		color: 0xedb2aa,
+		image: {
+			url: `https://i.imgur.com/7m2LO4M.png`,
+			height: 0,
+			width: 0,
+		},
+	};
+	//the bot will tag them in a welcome message and mention checking out #rules and #roles! 1078490033037770805
+	const ChannelIDs = {
+		NVOwelcome: "1081977778829795338",
+		NVOtestTestTesting: "962146899220131860",
+		ServerGeneral: "1048059530711404618",
+		DaniDeliaGeneral: "1078490033037770805",
+	};
+	let whereToSend;
+	if (member.guild.id === GuildIDs.DaniDeliaServer) {
+		whereToSend = ChannelIDs.DaniDeliaGeneral;
+	} else if (member.guild.id === GuildIDs.Server) {
+		whereToSend = ChannelIDs.ServerGeneral;
+	} else if (member.guild.id === GuildIDs.NVO) {
+		whereToSend = ChannelIDs.NVOwelcome;
+	}
+	client.channels.cache.get(whereToSend).send({ embeds: [embedMessage] });
+});
+
+//the following code makes leaf stack his own emoji on any leafemoji
+client.on("messageReactionAdd", async (reaction) => {
+	if (
+		//this is for Server:
+		reaction._emoji.id === "1079573401888358510" ||
+		//this is for NVO:
+		reaction._emoji.id === "1079573678515298344"
+	) {
+		// message.react("1079573401888358510")
+		const channel = await client.channels.fetch(reaction.message.channelId);
+		channel.messages.fetch(reaction.message.id).then((msg) => {
+			msg.react(reaction._emoji.id);
+		});
+	}
+});
 
 // log in and auhtenticate
 client.login(process.env.TOKEN);
-//"1078506917812506624" "1079448316321992715" "1079448313385996338" "1079448310982651975" "1079448308604485743"
 //(1079573401888358510 serverleaf) and (1079573678515298344 for NVO/akaServerID#938105437180551168)
